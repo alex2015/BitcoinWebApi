81f839d1b68e28a313b293769b8f90f37ddde3a4
alex2015 <alex2015@rambler.ru>
2018-10-30 03:01:19 +0300

    Add projects lib

---
+++ Data/LastBtcInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class LastBtcInfo
+    {
+        public DateTime Date { get; set; }
+        public string Address { get; set; }
+        public double Amount { get; set; }
+        public int Confirmation { get; set; }
+    }
+}
