4b6db76e5dc88c09e89e70e52c9d02710ee411a0
alex2015 <alex2015@rambler.ru>
2018-11-05 17:39:27 +0300

    add ef model

---
+++ Api/Controllers/BitcoinController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Data;
+
+namespace Api.Controllers
+{
+    public class BitcoinController : ApiController
+    {
+        [HttpPost]
+        public async Task<IHttpActionResult> SendBtc([FromBody] SendBtcInfo info)
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        public async Task<IHttpActionResult> GetLast()
+        {
+            return Ok();
+        }
+    }
+}
